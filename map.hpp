14a41325ddd7c0bf8466f481bf6d8bc27dfb12e4
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-03-12 21:45:21 +0800

    30%

--- map.hpp
+++ map.hpp
@@ -10,6 +10,288 @@
 #include "utility.hpp"
 #include "exceptions.hpp"
 
+namespace myContainer {
+    template<class T>
+    class AVLtree {
+    private:
+        struct Node {
+            T value;
+            int height = 0;
+            Node *leftson;
+            Node *rightson;
+            Node *parent;
+
+            Node() : leftson(nullptr), rightson(nullptr), parent(nullptr), height(0) {}
+
+            Node(const T &_value, Node *_parent) : value(_value), parent(_parent), leftson(nullptr),
+                                                   rightson(nullptr) {}
+
+            Node(const Node &o) = default;
+
+            inline int max(int x, int y) { return x > y ? x : y; }
+
+            void setHeight() {
+                int lh = this->leftson == nullptr ? -1 : this->leftson->height;
+                int rh = this->rightson == nullptr ? -1 : this->rightson->height;
+                this->height = max(lh, rh) + 1;
+            }
+
+            int getBalanceSize(){
+                int lh = this->leftson == nullptr ? -1 : this->leftson->height;
+                int rh = this->rightson == nullptr ? -1 : this->rightson->height;
+                return lh - rh;
+            }
+
+            void ll_rotation(){ // can't manage root proble
+                Node * _parent = this->parent;                //  check for nullptr
+                Node * _newroot_rs = this->leftson->rightson;      //  check for nullptr
+                Node * _newroot = this->leftson;
+                if(_parent){
+                    if (_parent->rightson == this) _parent->rightson = _newroot;
+                    else _parent->leftson = _newroot;
+                }
+                _newroot->parent = _parent;
+                _newroot->rightson = this;
+                this->parent = _newroot;
+                this->leftson = _newroot_rs;
+                if(_newroot_rs) _newroot_rs->parent = this;
+                this->setHeight(); _newroot->setHeight();
+            }
+
+            void rr_rotation(){
+                Node * _parent = this->parent;                //  check for nullptr
+                Node * _newroot_ls = this->rightson->leftson;      //  check for nullptr
+                Node * _newroot = this->rightson;
+                if(_parent){
+                    if (_parent->rightson == this) _parent->rightson = _newroot;
+                    else _parent->leftson = _newroot;
+                }
+                _newroot->parent = _parent;
+                _newroot->leftson = this;
+                this->parent = _newroot;
+                this->rightson = _newroot_ls;
+                if(_newroot_ls) _newroot_ls->parent = this;
+                this->setHeight(); _newroot->setHeight();
+            }
+
+            void lr_rotation(){
+                this->leftson->rr_rotation();
+                this->ll_rotation();
+            }
+
+            void rl_rotation(){
+                this->rightson->ll_rotation();
+                this->rr_rotation();
+            }
+        };
+
+        Node *root;
+        int nodeNum;
+
+        Node *getMax(Node *_root) { return _root->rightson == nullptr ? _root : getMax(_root->rightson);}
+
+        Node *getMin(Node *_root) {return _root->leftson == nullptr ? _root : getMin(_root->leftson);}
+
+        Node *getSuccessor(Node *_root) {
+            if (_root == getMax(this->root)) return nullptr;
+            if (_root->rightson != nullptr) return getMin(_root->rightson);
+            else {
+                Node *p = _root->parent;
+                Node *q = _root;
+                while (p != nullptr && p->rightson == q) {
+                    q = p;
+                    p = p->parent;
+                }
+                return p;
+            }
+        }
+
+        Node *getPredecessor(Node *_root) {
+            if (_root == getMin(this->root)) return nullptr;
+            if (_root->leftson != nullptr) return getMax(_root->leftson);
+            else {
+                Node *p = _root->parent, *q = _root;
+                while (p != nullptr && p->leftson == q) {
+                    q = p, p = p->parent;
+                }
+                return p;
+            }
+        }
+
+        void clear(Node *_root) {
+            if (_root == nullptr) return;
+            if (_root->leftson != nullptr) clear(_root->leftson);
+            if (_root->rightson != nullptr) clear(_root->rightson);
+            delete _root;
+        }
+
+        // usage: root = copy(root,o.root);
+        Node *copy(Node *duplicate, Node *_parent = nullptr) {
+            if (duplicate == nullptr) return duplicate;
+            Node *newNode = new Node(duplicate->value, _parent);
+            newNode->leftson = copy(duplicate->leftson, newNode);
+            newNode->rightson = copy(duplicate->rightson, newNode);
+            return newNode;
+        }
+
+        Node *search(Node *_root, const T &_value) {
+            if (_root == nullptr) return nullptr;
+            if (_root->value == _value) return _root;
+            if (_root->value > _value) return search(_root->leftson, _value);
+            if (_root->value < _value) return search(_root->rightson, _value);
+        }
+
+        void assistant_insert(Node *_root, const T &_value) {
+            if (_root->value < _value) {
+                if (_root->rightson == nullptr) _root->rightson = new Node(_value, _root);
+                else assistant_insert(_root->rightson, _value);
+            } else {
+                if (_root->leftson == nullptr) _root->leftson = new Node(_value, _root);
+                else assistant_insert(_root->leftson, _value);
+            }
+            _root->setHeight();
+            this->balance(_root);
+        }
+
+        void assistant_height(Node *_leaf) {
+            while (_leaf != nullptr) {
+//                int tmpheight = _leaf->height;
+                _leaf->setHeight();
+                Node * tmp = _leaf;
+                _leaf = _leaf->parent;
+                this->balance(tmp);
+            }
+        }
+
+        void balance(Node * _root){
+            bool isRoot = (_root == this->root);
+            if(_root->getBalanceSize() == 2){
+                if(_root->leftson->getBalanceSize() >= 0){ // 为什么删除时左右相等要单旋转？
+                    if(isRoot) this->root = _root->leftson;
+                    _root->ll_rotation();
+                }else{
+                    if (isRoot) this->root = _root->leftson->rightson;
+                    _root->lr_rotation();
+                }
+            }else if(_root->getBalanceSize() == -2){
+                if(_root->rightson->getBalanceSize() <= 0){
+                    if(isRoot) this->root = _root->rightson;
+                    _root->rr_rotation();
+                }
+                else{
+                    if(isRoot) this->root = _root->rightson->leftson;
+                    _root->rl_rotation();
+                }
+            }
+        }
+    public:
+        AVLtree() : root(nullptr), nodeNum(0) {}
+
+        AVLtree(const AVLtree &o) {
+            this->nodeNum = o.nodeNum;
+            this->root = copy(o.root);
+        }
+
+        ~AVLtree() { clear(this->root); }
+
+        AVLtree &operator=(const AVLtree &o) {
+            if (this == &o) return *this;
+            this->nodeNum = o.nodeNum;
+            clear(this->root);
+            this->root = copy(o.root);
+            return *this;
+        }
+
+        bool insert(const T &_value) {
+            if (search(this->root, _value) != nullptr) return false;
+            if (!nodeNum) this->root = new Node(_value, nullptr);
+            else {
+                assistant_insert(this->root, _value);
+            }
+            nodeNum++;
+            return true;
+        }
+
+        bool erase(const T &_value, Node *_target = nullptr) {
+            Node *target = (_target == nullptr) ? search(this->root, _value) : _target;
+            if (target == nullptr) return false;
+            Node *_parent = target->parent;
+            bool isRoot = (target == this->root);
+            //case1:target don't own either ls nor rs;
+            if (!target->leftson && !target->rightson) {
+                if (_parent) {
+                    if (_parent->rightson == target) _parent->rightson = nullptr;
+                    else _parent->leftson = nullptr;
+                }
+                delete target;
+                nodeNum--;
+                if (isRoot) this->root = nullptr;
+                assistant_height(_parent);
+                return true;
+            }
+            //case2:target only owns one child node
+            if (!target->leftson) {
+                if (_parent) {
+                    if (_parent->rightson == target) _parent->rightson = target->rightson;
+                    else _parent->leftson = target->rightson;
+                }
+                target->rightson->parent = _parent;
+                if (isRoot) this->root = target->rightson;
+                assistant_height(_parent);
+                delete target;
+                nodeNum--;
+                return true;
+            }
+            if (!target->rightson) {
+                if (_parent) {
+                    if (_parent->rightson == target) _parent->rightson = target->leftson;
+                    else _parent->leftson = target->leftson;
+                }
+                target->leftson->parent = _parent;
+                if (isRoot) this->root = target->leftson;
+                assistant_height(_parent);
+                delete target;
+                nodeNum--;
+                return true;
+            }
+            //case3:target owns two sons
+            if (target->rightson && target->leftson) {
+                Node *_successor = getSuccessor(target);
+                T tmpValue(_successor->value);
+                erase(tmpValue, _successor);
+                target->value = tmpValue;
+                return true;
+            }
+        }
+
+        void clear(){
+            clear(this->root);
+            this->nodeNum = 0;
+        }
+        // for test
+        void print() {
+            Node *tmp = getMin(this->root);
+            while (tmp != nullptr) {
+                std::cout << "value : " << tmp->value << " height : " << tmp->height << std::endl;
+                tmp = getSuccessor(tmp);
+            }
+        }
+        Node * getRoot(){return root;}
+        void inorderTravel(Node * _root){
+            if(_root == nullptr) return;
+            inorderTravel(_root->leftson);
+            std::cout << "value : " << _root->value << " height : " << _root->height << std::endl;
+            inorderTravel(_root->rightson);
+        }
+        void preorderTravel(Node * _root){
+            if(_root == nullptr) return;
+            std::cout << "value : " << _root->value << " height : " << _root->height << std::endl;
+            preorderTravel(_root->leftson);
+            preorderTravel(_root->rightson);
+        }
+    };
+}
+
 namespace sjtu {
 
 template<
